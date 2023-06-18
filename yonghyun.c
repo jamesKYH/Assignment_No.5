4bcf18daaa43f79201639d08c96cf89d7d7b4242
jamesKYH2 <raykim9777-2@gmail.com>
2023-06-18 20:41:30 +0900

    고객정보 저장함수 추가

--- yonghyun.c
+++ yonghyun.c
@@ -9,6 +9,25 @@ typedef struct customer{
     char *phone;
 }CUSTMOR;
 
+void save_info(int n,struct customer *p){
+    int i,j;
+    int len1,len2;
+    char temp1[100],temp2[100];
+    for(i=0;i<n;i++){
+        scanf("%s %s",temp1,temp2);
+        len1=(int)strlen(temp1);
+        len2=(int)strlen(temp2);
+        p[i].name=(char *)malloc((len1+1)*sizeof(char));
+        p[i].phone=(char *)malloc((len2+1)*sizeof(char));
+        strcpy(p[i].name,temp1);
+        strcpy(p[i].phone,temp2);
+        int k=9;
+        for(j=0;j<5;j++){
+            p[i].id[j]=p[i].phone[k];
+            k+=1;
+        }
+    }
+}
 
 int main(void) {
     //고객 수 입력
